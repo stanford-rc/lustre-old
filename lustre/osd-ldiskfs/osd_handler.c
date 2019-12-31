712dcdc3ceda5c7e65d55cf965ee9003159496cb
Alexander Boyko <c17825@cray.com>
2019-12-31 10:17:58 -0500

    LU-13093 osd: fix osd_attr_set race
    The race between tgt_brw_write->ofd_write_attr_set and
    ofd_attr_set took a place, and it could set a wrong attributes.
    ofd_write_attr_set() does checks and declarations and sleeps on
    ofd_read_lock. Another thread executes ofd_attr_set() and sets
    initial uid/gid. After that the first thread wakeups and sets
    another uid/gid. But ofd_write_attr_set should change attributes
    for initial time only.
    This also leads to a bug at credits check cause uid was changed
    between declaration and attr_set.
    
    osd_trans_exec_check(ATTR_SET) has a wrong place when xattr_set
    is called. Also xattr doesn't have osd_trans_exec_op.
    
    lustre-OST0001: opcode 0: used 9, used now 9, reserved 1
    create: 0/0/0, destroy: 0/0/0
    attr_set: 1/1/9, xattr_set: 2/274/0
    write: 0/0/0, punch: 0/0/0, quota 6/6/0
    insert: 0/0/0, delete: 0/0/0
    ref_add: 0/0/0, ref_del: 0/0/0
    LBUG
    
    Lustre-change: https://review.whamcloud.com/37117
    Lustre-commit: 83ac160bd19bcedaa817a419a3912726b9a3bd9d
    
    Cray-bug-id: LUS-8133
    Fixes: 9f79d4488  ("LU-10048 ofd: take local locks within transaction")
    Signed-off-by: Alexander Boyko <c17825@cray.com>
    Change-Id: Id36ff633b0d97fff345ec105e0aa1b14fccafce4
    Reviewed-by: Andrew Perepechko <c17827@cray.com>
    Reviewed-by: Andriy Skulysh <c17819@cray.com>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>
    Signed-off-by: Etienne AUJAMES <eaujames@ddn.com>

--- lustre/osd-ldiskfs/osd_handler.c
+++ lustre/osd-ldiskfs/osd_handler.c
@@ -2895,9 +2895,10 @@ static int osd_declare_attr_set(const struct lu_env *env,
 			RETURN(rc);
 
 		gid = i_gid_read(obj->oo_inode);
+		CDEBUG(D_QUOTA, "declare uid %d -> %d gid %d -> %d\n", uid,
+		       attr->la_uid, gid, attr->la_gid);
 		enforce = (attr->la_valid & LA_GID) && (attr->la_gid != gid);
-		rc = osd_declare_attr_qid(env, obj, oh, bspace,
-					  i_gid_read(obj->oo_inode),
+		rc = osd_declare_attr_qid(env, obj, oh, bspace, gid,
 					  attr->la_gid, enforce, GRPQUOTA,
 					  ignore_edquot);
 		if (rc)
@@ -3051,6 +3052,11 @@ static int osd_quota_transfer(struct inode *inode, const struct lu_attr *attr,
 	    (attr->la_valid & LA_GID && attr->la_gid != i_gid_read(inode))) {
 		struct iattr iattr;
 
+		CDEBUG(D_QUOTA,
+		       "executing dquot_transfer inode %ld uid %d -> %d gid %d -> %d\n",
+		       inode->i_ino, i_uid_read(inode), attr->la_uid,
+		       i_gid_read(inode), attr->la_gid);
+
 		ll_vfs_dq_init(inode);
 		iattr.ia_valid = 0;
 		if (attr->la_valid & LA_UID)
@@ -3146,6 +3152,8 @@ static int osd_attr_set(const struct lu_env *env,
 
 	ll_dirty_inode(inode, I_DIRTY_DATASYNC);
 
+	osd_trans_exec_check(env, handle, OSD_OT_ATTR_SET);
+
 	if (!(attr->la_valid & LA_FLAGS))
 		GOTO(out, rc);
 
@@ -3164,6 +3172,9 @@ static int osd_attr_set(const struct lu_env *env,
 		lma->lma_incompat |=
 			lustre_to_lma_flags(attr->la_flags);
 		lustre_lma_swab(lma);
+
+		osd_trans_exec_op(env, handle, OSD_OT_XATTR_SET);
+
 		rc = __osd_xattr_set(info, inode, XATTR_NAME_LMA,
 				     lma, sizeof(*lma), XATTR_REPLACE);
 		if (rc != 0) {
@@ -3179,7 +3190,6 @@ static int osd_attr_set(const struct lu_env *env,
 		osd_trans_exec_check(env, handle, OSD_OT_XATTR_SET);
 	}
 out:
-	osd_trans_exec_check(env, handle, OSD_OT_ATTR_SET);
 
 	return rc;
 }
